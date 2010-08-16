88ac662148cf234681e1b0c24c73ab9db0e279c3
Danielle Marcos <danielle.marcos@gmail.com>
2010-08-16 16:44:45 -0400

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,12 +8,24 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+  
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                      :content => " | Home")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                      :content => " | Contact")
     end
   end
   
@@ -22,13 +34,11 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-  end
-
-  describe "GET 'figs'" do
-    it "should be successful" do
-      get 'figs'
-      response.should be_success
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                      :content => " | About")
     end
   end
-
 end
